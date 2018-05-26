cf752dbedd7e024ad4a23bc1e4a20ef1c5ee3e66
deepak <Deepak Acharya>
2018-05-26 22:16:37 +0545

    remove unused variables

--- frontend/src/routers/AppRouter.js
+++ frontend/src/routers/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Header from '../components/Header';
 import DashboardPage from '../components/DashboardPage';
