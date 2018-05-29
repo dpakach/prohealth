25e32ed93687a358ba0bea5d9ce55446c3a91a0d
divyamani <dma.htd@gmail.com>
2018-05-30 00:50:21 +0545

    Add auth urls

--- backend/user_profile/urls.py
+++ backend/user_profile/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import url
 from django.conf.urls import include
 from rest_framework.routers import DefaultRouter
+from django.views.generic import TemplateView
 
 from . import views
 
 router = DefaultRouter()
 router.register('profile', views.UserProfileViewSet)
-router.register('login', views.LoginViewSet, base_name='login')
+# router.register('login', views.LoginViewSet, base_name='login')
 urlpatterns = [
-    url(r'', include(router.urls))
+    url(r'', include(router.urls)),
+    url(r'auth/', include('rest_auth.urls')),
+    url(r'^password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        TemplateView.as_view(template_name="password_reset_confirm.html"),
+        name='password_reset_confirm'),
 ]
