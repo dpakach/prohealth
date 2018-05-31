4aa078b424aa02e6a2951c5af7dfdc4b81615c60
Deepak Acharya <dpakach@gmail.com>
2018-05-31 22:12:23 +0545

    production pipeline failed fix-1

--- frontend/src/components/query/QueryListItemComponent.js
+++ frontend/src/components/query/QueryListItemComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List, Avatar, Button, Spin } from 'antd';
+import { List, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 
 const QueryListItemComponent = (props) => (
