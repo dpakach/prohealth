67cd444b1a253130fa225b1aadbde4f948ebf3a7
Anjan Chandra Paudel <paudelanjanchandra@gmail.com>
2018-06-03 17:32:02 +0545

    maintaining isauthenticated but failed

--- backend/users_query/views.py
+++ backend/users_query/views.py
@@ -1,7 +1,9 @@
+
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.permissions import IsAuthenticated
 
 
 from . models import UserQuery
