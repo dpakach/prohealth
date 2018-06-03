67dff743535ef57a9f265a9c22510996e555fdec
Anjan Chandra Paudel <paudelanjanchandra@gmail.com>
2018-06-03 16:23:27 +0545

    added permissions and authentication to users_query

--- backend/users_query/views.py
+++ backend/users_query/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 
 from . models import UserQuery
 from . serializer import UserQuerySerializer
+from . import permissions
 # Create your views here.
 
 class UserQueryViewset(viewsets.ModelViewSet):
     
     serializer_class = UserQuerySerializer
     queryset = UserQuery.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnUserQuery,)
