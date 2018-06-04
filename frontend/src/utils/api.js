59aaf1091732320673290f7d969a8527b1da22da
deepak <Deepak Acharya>
2018-06-04 15:22:30 +0545

    login using axios

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,19 +4,27 @@ import history from './historyUtils';
 import axios from 'axios';
 
 export const login = (form_data) => {
-    fetch(AuthUrls.LOGIN, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(form_data),
-    }).then(response => {
-        console.log(response);
-        return response.json();
-    }).then(data => {
-        console.log(data);
-        localStorage.setItem('authentication', data['key']);
-    });
+
+    axios.post(AuthUrls.LOGIN, form_data)
+        .then(response => {
+            const token = response.data.key;
+            localStorage.setItem('authentication', token);
+        }).catch(error => {
+            console.log(error);
+        });
+    // fetch(AuthUrls.LOGIN, {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(form_data),
+    // }).then(response => {
+    //     console.log(response);
+    //     return response.json();
+    // }).then(data => {
+    //     console.log(data);
+    //     localStorage.setItem('authentication', data['key']);
+    // });
 };
 
 
