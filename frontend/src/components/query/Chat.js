b56c05735a8ff89b37e27272c7d2eee2473fb5b6
Deepak Acharya <dpakach@gmail.com>
2018-06-07 20:46:14 +0545

    working on custom themes

--- frontend/src/components/query/Chat.js
+++ frontend/src/components/query/Chat.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Input, Button } from 'antd';
+
 
 const Chat = props => (
     <div className="chat">
+        <div className="chat__input">
+            <Input 
+                placeholder="enter your message"
+                size="large"
+            />
+            <Button type="primary">send</Button>
+
+        </div>
         <div className="chat__body">
             <div className="chat__message chat__message__self">Hi there</div>
             <div className="chat__message">Hello hou you doin</div>
@@ -21,13 +31,11 @@ const Chat = props => (
                 a lot of things
             </div>
         </div>
-        <div className="chat__input">
-            <input className="chat__input--text " type="text" />
-            <button className="btn btn--primary chat__input--button">
-                Send
-            </button>
-        </div>
     </div>
 );
 
 export default Chat;
+// <input className="chat__input--text " type="text" />
+ //           <button className="btn btn--primary chat__input--button">
+   //             Send
+     //       </button>
