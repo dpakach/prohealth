db9a30cb8525da14db68483f0287e91685114387
Anjan Chandra Paudel <paudelanjanchandra@gmail.com>
2018-06-07 15:51:41 +0545

    primary key fields watching for changes

--- backend/users_query/models.py
+++ backend/users_query/models.py
@@ -2,6 +2,37 @@ from django.db import models
 from user_profile.models import UserProfile
 # Create your models here.
 
+import uuid
+import os
+
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('user_query', filename)
+
+class Medicine(models.Model):
+    name_of_medicine = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    times_a_day = models.IntegerField()
+    remarks = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name_of_medicine
+    
+
+
+    
+
+class Appointment(models.Model):
+    appointed_doc = models.CharField(max_length=25)
+    appointed_time = models.DateTimeField()
+    hospital = models.CharField(max_length=25)
+    venue = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.hospital
+
+
 class UserQuery(models.Model):
     """ class used to create users health related queries"""
 
@@ -17,9 +48,14 @@ class UserQuery(models.Model):
     weight_of_patient = models.SmallIntegerField(blank = True, null=True)
     title_problem = models.CharField(max_length = 510)
     description  = models.TextField()
+    file_related = models.FileField(blank=True, upload_to=get_file_path)
     tag = models.CharField(max_length = 1, choices = TAG_CHOICES)
     date_of_submission = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(UserProfile)
+    # user = models.ForeignKey(UserProfile)
+    taken = models.BooleanField(default=False)
+    resolved = models.BooleanField(default=False)
+    #appointment = models.OneToOneField(Appointment, blank=True)
+
 
     def __str__(self):
         return self.title_problem
@@ -29,3 +65,12 @@ class UserQuery(models.Model):
         obj.user = request.user
         super().save_model(request, obj, form, change)
 
+
+class Prescription(models.Model):
+    medicine = models.ManyToManyField(Medicine)
+    description = models.TextField()
+    prescribed_date = models.DateTimeField(auto_now_add=True)
+    query = models.ForeignKey(UserQuery)
+
+    def __str__(self):
+        return str(self.prescribed_date)
