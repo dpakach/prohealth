ed3879f4cacfa90a3df2aa77b4aa809284c07b0f
Anjan Chandra Paudel <paudelanjanchandra@gmail.com>
2018-06-22 14:02:54 +0545

    Add medicine as prescribe view and delete method for all fixed

--- backend/users_query/serializer.py
+++ backend/users_query/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers, fields
 from rest_framework.serializers import PrimaryKeyRelatedField
 
-from . models import UserQuery, Medicine, Appointment, Prescription
+from . models import UserQuery, Medicine, Appointment
 
 class UserQuerySerializer(serializers.ModelSerializer):
     """ helps to serialize the data realted to UserQuery Model"""
@@ -16,44 +16,20 @@ class UserQuerySerializer(serializers.ModelSerializer):
     class Meta:
         model = UserQuery
         fields = '__all__'
-        # read_only_fields = ('user',)
-
-    # def validate_user(self, user):
-    #     """
-    #     Validate authenticated user
-    #     """
-
-    #     if user != self.context['request'].user:
-    #         raise serializers.ValidationError('You can not create post replies for other users')
-    #     return user
+    
     
 class MedicineSerializer(serializers.ModelSerializer):
-    # queryset = Medicine.objects.all()
+
     class Meta:
         model = Medicine
         fields = '__all__'
-
-
-class PrescriptionSerializer(serializers.ModelSerializer):
-    
-    medicine = MedicineSerializer(many=True, read_only=False)
-    class Meta:
-        model = Prescription
-        fields = ('id','medicine','description','prescribed_date')
-
-    def create(self, validate_data):
-        medicine_data = validate_data.pop('medicine')
-        prescription = Prescription.objects.create(**validate_data)
-        med_list = []
-        for medicine_details in medicine_data:
-            med_list.append(Medicine.objects.create(**medicine_details))
-        prescription.medicine.add(*med_list)
-        return prescription
-    
+        read_only_fields = ('query',)
 
 
 class AppointmentSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Appointment
         fields = '__all__'
-        read_only_fields = ('user',)
+        read_only_fields = ('query',)
+
--- backend/users_query/urls.py
+++ backend/users_query/urls.py
@@ -8,8 +8,8 @@ from . import views
 urlpatterns = [
     url(r'^query$',views.UserQueryView.as_view()),
     url(r'^query/(?P<query_id>[\d]+)$',views.UserQueryDetailView.as_view()),
-    url(r'^query/(?P<query_id>[\d]+)/prescribe$',views.PrescriptionView.as_view()),
-    url(r'^query/(?P<query_id>[\d]+)/prescribe/1$',views.PrescriptionDetailView.as_view()),
+    url(r'^query/(?P<query_id>[\d]+)/prescribe$',views.PrescribeView.as_view()),
+    url(r'^query/(?P<query_id>[\d]+)/prescribe/(?P<prescribe_id>[\d]+)$',views.PrescribeDetailView.as_view()),
     url(r'^query/(?P<query_id>[\d]+)/appoint$',views.AppointmentView.as_view()),
     url(r'^query/(?P<query_id>[\d]+)/appoint/1$',views.AppointmentDetailView.as_view()),
 
