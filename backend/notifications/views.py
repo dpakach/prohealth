37b88192291693a1a1b5fe3152577f9fdd716663
divyamani <dma.htd@gmail.com>
2018-06-27 20:05:16 +0545

    Add read all notifications

--- backend/notifications/views.py
+++ backend/notifications/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .models import Notification
 from rest_framework import permissions
 from .serializers import NotificationSerializer
+from rest_framework.decorators import api_view
 
 class NotificationView(APIView):
     serializer_class = NotificationSerializer
@@ -25,3 +26,11 @@ class NotificationView(APIView):
         notification = Notification(user=request.user, message=message, title=title)
         notification.save()
         return Response('Notification Added.')
+
+@api_view(['GET', 'POST'])
+def NotificationsReadView(request):
+    notifications = Notification.objects.filter(user=request.user)
+    for notification in notifications:
+        notification.read_notification()
+        notification.save()
+    return Response('All notifications read.')
