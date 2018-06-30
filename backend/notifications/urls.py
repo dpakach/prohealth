d7a9642bed42e69c84d12647571d8b55ce4b3695
divyamani <dma.htd@gmail.com>
2018-06-30 23:43:00 +0545

    Add Read all notifications for particular queries

--- backend/notifications/urls.py
+++ backend/notifications/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.conf.urls import include
-from .views import NotificationView, NotificationsReadView, NotificationDetailView, SingleNotificationsReadView
+from .views import NotificationView, NotificationsReadView, NotificationDetailView, SingleNotificationsReadView, QueryNotificationsReadView
 
 urlpatterns = [
     url(r'^notifications/$', NotificationView.as_view()),
     url(r'^notifications/read_all/$', NotificationsReadView),
+    url(r'^notifications/read_all/(?P<query_id>[\d]+)/$', QueryNotificationsReadView),
     url(r'^notifications/(?P<notification_id>[\d]+)/$', NotificationDetailView.as_view()),
     url(r'^notifications/(?P<notification_id>[\d]+)/read/$', SingleNotificationsReadView),
 ]
