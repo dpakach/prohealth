bf569828e5b816e494fd95564a94bfb75f4d45d2
divyamani <dma.htd@gmail.com>
2018-07-02 20:46:41 +0545

    Fix notification created date

--- backend/notifications/urls.py
+++ backend/notifications/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 from django.conf.urls import include
-from .views import NotificationView, NotificationsReadView, NotificationDetailView, SingleNotificationsReadView, QueryNotificationsReadView, get_notification_age
+from .views import NotificationView, NotificationsReadView, NotificationDetailView, SingleNotificationsReadView, QueryNotificationsReadView
 
 urlpatterns = [
     url(r'^notifications/$', NotificationView.as_view()),
     url(r'^notifications/read_all/$', NotificationsReadView),
     url(r'^notifications/read_all/(?P<query_id>[\d]+)/$', QueryNotificationsReadView),
     url(r'^notifications/(?P<notification_id>[\d]+)/$', NotificationDetailView.as_view()),
-    url(r'^notifications/(?P<notification_id>[\d]+)/get_age/$', get_notification_age),
+    # url(r'^notifications/(?P<notification_id>[\d]+)/get_age/$', get_notification_age),
     url(r'^notifications/(?P<notification_id>[\d]+)/read/$', SingleNotificationsReadView),
 ]
