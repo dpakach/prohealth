5d414e3447f8132e357efb06b45abc7b15db3c37
divyamani <dma.htd@gmail.com>
2018-07-11 09:54:59 +0545

    Add Chat Views and URLs

--- backend/chats/models.py
+++ backend/chats/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 from users_query.models import UserQuery
 
 class Message(models.Model):
@@ -8,12 +9,12 @@ class Message(models.Model):
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_by')
     query = models.ForeignKey(UserQuery, on_delete=models.CASCADE)
     read = models.BooleanField(default=False)
-    date_time = models.DateTimeField(editable=False)
+    created = models.DateTimeField(editable=False)
 
     def save(self, *args, **kwargs):
         if not self.id:
             self.created = timezone.now()
-        return super(Chat, self).save(*args, **kwargs)
+        return super(Message, self).save(*args, **kwargs)
 
     def read_message(self):
         self.read = True
@@ -22,7 +23,7 @@ class Message(models.Model):
         self.read = False
 
     def __str__(self):
-        return self.sender + "to" + self.receiver + "on" + self.date_time
+        return str(self.sender) + " to " + str(self.receiver) + " on " + str(self.created)
 
 
     
--- backend/chats/views.py
+++ backend/chats/views.py
@@ -1,3 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Message
+from .serializers import MessageSerializer
+from rest_framework import permissions
+from users_query.models import UserQuery
+from rest_framework.decorators import api_view
 
-# Create your views here.
+class SentMessageView(APIView):
+    serializer_class = MessageSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    @staticmethod
+    def get(request):
+        """
+        List Messages for particular user
+        """
+
+        sent_messages = Message.objects.filter(sender=request.user)
+        return Response(MessageSerializer(sent_messages, many=True).data)
+
+    @staticmethod
+    def post(request):
+        receiver = request.data.get('receiver')
+        message = request.data.get('message')
+        query = get_object_or_404(UserQuery, id=request.data.get('query'))
+        message = Message(sender=request.user, message=message, receiver=receiver, query=query)
+        message.save()
+        return Response('Message sent.')
+
+class ReceivedMessageView(APIView):
+    serializer_class = MessageSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    @staticmethod
+    def get(request):
+        """
+        List Messages for particular user
+        """
+
+        received_messages = Message.objects.filter(receiver=request.user)
+        return Response(MessageSerializer(received_messages, many=True).data)
+
+
+class SentMessageDetailView(APIView):
+
+    @staticmethod
+    def get(request, message_id):
+        message = get_object_or_404(Message, id=message_id, sender=request.user)
+        return Response(MessageSerializer(message).data)
+
+class ReceivedMessageDetailView(APIView):
+
+    @staticmethod
+    def get(request, message_id):
+        message = get_object_or_404(Message, id=message_id, receiver=request.user)
+        return Response(MessageSerializer(message).data)
+
+
+class QueryMessageView(APIView):
+    serializer_class = MessageSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    @staticmethod
+    def get(request, query_id):
+        """
+        List Messages for particular user
+        """
+
+        messages = Message.objects.filter(query=get_object_or_404(UserQuery, id=query_id))
+        return Response(MessageSerializer(messages, many=True).data)
+
+    # @staticmethod
+    # def post(request):
+    #     receiver = request.data.get('receiver')
+    #     message = request.data.get('message')
+    #     query = get_object_or_404(UserQuery, id=request.data.get('query'))
+    #     message = Message(sender=request.user, message=message, receiver=receiver, query=query)
+    #     message.save()
+    #     return Response('Message sent.')
