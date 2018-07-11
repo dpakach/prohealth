6f01fe54a3c5f44af0b559f33d59b204b76e78e1
divyamani <dma.htd@gmail.com>
2018-07-12 00:39:48 +0545

    Add Chat views

--- backend/chats/views.py
+++ backend/chats/views.py
@@ -20,14 +20,6 @@ class SentMessageView(APIView):
         sent_messages = Message.objects.filter(sender=request.user)
         return Response(MessageSerializer(sent_messages, many=True).data)
 
-    @staticmethod
-    def post(request):
-        receiver = request.data.get('receiver')
-        message = request.data.get('message')
-        query = get_object_or_404(UserQuery, id=request.data.get('query'))
-        message = Message(sender=request.user, message=message, receiver=receiver, query=query)
-        message.save()
-        return Response('Message sent.')
 
 class ReceivedMessageView(APIView):
     serializer_class = MessageSerializer
@@ -65,17 +57,45 @@ class QueryMessageView(APIView):
     @staticmethod
     def get(request, query_id):
         """
-        List Messages for particular user
+        List Messages for particular user in given query
         """
 
         messages = Message.objects.filter(query=get_object_or_404(UserQuery, id=query_id))
         return Response(MessageSerializer(messages, many=True).data)
 
-    # @staticmethod
-    # def post(request):
-    #     receiver = request.data.get('receiver')
-    #     message = request.data.get('message')
-    #     query = get_object_or_404(UserQuery, id=request.data.get('query'))
-    #     message = Message(sender=request.user, message=message, receiver=receiver, query=query)
-    #     message.save()
-    #     return Response('Message sent.')
+    @staticmethod
+    def post(request):
+        user = request.user
+        query = get_object_or_404(UserQuery, id=request.data.get('query'))
+        if user.is_doctor:
+            receiver = query.user
+        else:
+            receiver = query.taken_by
+        message = request.data.get('message')
+        message = Message(sender=request.user, message=message, receiver=receiver, query=query)
+        message.save()
+        return Response('Message sent.')
+
+@api_view(['GET'])
+def MessagesReadView(request):
+    messages = Message.objects.filter(receiver=request.user)
+    for message in messages:
+        message.read_message()
+        message.save()
+    return Response('All messages read.')
+
+@api_view(['GET'])
+def QueryMessagesReadView(request, query_id):
+    query=get_object_or_404(UserQuery, id=query_id)
+    messages = Message.objects.filter(query=query, receiver=request.user)
+    for message in messages:
+        print(message)
+        message.read_message()
+        message.save()
+    return Response('Messages for given query read.')
+
+@api_view(['GET'])
+def get_message_age(request, message_id):
+    message = get_object_or_404(Message, id=message_id)
+    created_date = message.created
+    return Response(created_date)
