22d7da6c61fd4236c60ee227c8d3b63a18822cd7
Deepak Acharya <dpakach@gmail.com>
2018-07-13 22:43:09 +0545

    Add Lightbox and styling for edit form

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,5 +5,7 @@ import App from './App';
 
 import './styles/style.css';
 import './styles/main.scss';
+import 'react-image-lightbox/style.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
